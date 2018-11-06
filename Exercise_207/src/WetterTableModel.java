bbcd3887155fbc62c4ae73c6c2e2fee2a9f79184
David Blümel <bludac16@htl-kaindorf.ac.at>
2018-11-06 09:12:31 +0100

    finished class: wetterstation, working on: WetterTableModel

---
+++ Exercise_207/src/WetterTableModel.java
@@ -0,0 +1,34 @@
+
+import java.util.LinkedList;
+import javax.swing.table.AbstractTableModel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author David
+ */
+public class WetterTableModel extends AbstractTableModel{
+    private String[] colNames = {"Place", "Sea Level", "Temperature", "rel. Humidity"};
+    private LinkedList<Wetterstation> wetterstationen = new LinkedList<>();
+
+    @Override
+    public int getRowCount() {
+        return wetterstationen.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return colNames.length;
+    }
+
+    @Override
+    public Object getValueAt(int row, int col) {
+        return wetterstationen.get(row);
+    }
+    
+}
